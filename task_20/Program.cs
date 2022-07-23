618118d9461ceb2e536520296706ff6d224dde94
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-23 20:40:39 +0300

    updated code

--- task_20/Program.cs
+++ task_20/Program.cs
@@ -8,13 +8,13 @@ A (7,-5); B (1,-1) -> 7,21*/
 
 Console.Clear();
 int firstX, firstY, secondX, secondY;
-Console.WriteLine("Введите координаты X первой точки: ");
+Console.Write("Введите координаты X первой точки: ");
 firstX = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты X первой точки: ");
+Console.Write("Введите координаты Y первой точки: ");
 firstY = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты X первой точки: ");
+Console.Write("Введите координаты X второй точки: ");
 secondX = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты X первой точки: ");
+Console.Write("Введите координаты Y второй точки: ");
 secondY = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(GetDistance(firstX, firstY, secondX, secondY));
@@ -23,3 +23,5 @@ double GetDistance(int ax, int ay, int bx, int by)
 {
     return Math.Round(Math.Sqrt(((bx - ax) * (bx - ax)) + ((by - ay) * (by - ay))), 2, MidpointRounding.ToZero);
 }
+
+Console.WriteLine();
