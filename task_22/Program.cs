1120de4df7d0221926ea8c43c97adb2c1e879e9e
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-23 21:44:35 +0300

    updated code

--- task_22/Program.cs
+++ task_22/Program.cs
@@ -4,8 +4,10 @@
 5 -> 1, 4, 9, 16, 25.
 2 -> 1,4*/
 
-Console.Write("Введите число:");
+Console.Clear();
+Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
+int i=1;
 
 WriteSquareTable(n);
 
