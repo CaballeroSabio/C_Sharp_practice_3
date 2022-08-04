9739af319b34772e6dd50879affac435f8b595c7
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-08-04 19:25:52 +0300

    created the new code

---
+++ task_23/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Clear();
+Console.WriteLine("Hi, User!");
+Console.Write("Enter a number from 1 to infinity: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+if (n > 0)
+{
+    WriteSquareTable(n);
+}
+else
+{
+    Console.WriteLine("Wrong number entered!");
+}
+
+
+void WriteSquareTable(int n)
+{
+    int i = 1;
+    
+    while (i <= n)
+    {
+        Console.WriteLine($"| {i} | {i * i * i, 3} |");
+        i++;
+    }
+}
+
+Console.WriteLine("Goodbye, User!");
+Console.WriteLine();
